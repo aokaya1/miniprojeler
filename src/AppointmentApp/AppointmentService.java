652525bd0d5bcc7276829583b23b6e821385e121
Ahmet SAAT <avahmetsaat@gmail.com>
2023-02-02 00:24:25 +0300

    02/02/2023

--- src/AppointmentApp/AppointmentService.java
+++ src/AppointmentApp/AppointmentService.java
@@ -20,8 +20,9 @@ public class AppointmentService {
             Doktor doktor = doctorservice.getDoctorById(selectDoktor);
             //14.adim tarih secme/tarih listesinde randevu alinmayan yer var mi
             if (!doktor.getDates().isEmpty()) {
-                System.out.println("Lutfen Ismini Giriniz");
+                System.out.println("Lutfen Isminizi Giriniz");
                 String name = input.nextLine();
+                name=input.nextLine();
                 System.out.println("Sayin" + name + "Randevu alabileceginiz tarihler");
                 for (int i = 0; i < doktor.getDates().size(); i++) {
                     System.out.println((i + 1) + " - " + doktor.getDates().get(i));
@@ -33,11 +34,13 @@ public class AppointmentService {
                     Appointment appointment = new Appointment(name, doktor, doktor.getDates().get(select - 1));
                     this.appointments.add(appointment);
                     System.out.println("Sayin " + name + " Randevu tarihiniz = " + doktor.getDates().get(select - 1));
-                    
+                    System.out.println("Randevu no = " + appointment.getId() + " ile randevu bilgilerinizi goruntuleyebilirsiniz");
+
+                    //16.adim olusturulan randevu tarihini sil
+                    doktor.getDates().remove(select - 1);
                 } else {
                     System.out.println("Hatali Giris");
                 }
-
             } else {
                 System.out.println(doktor.getName() + "in bu hafta tum randevulri dolmustur");
             }
@@ -47,5 +50,33 @@ public class AppointmentService {
 
 
     }
+    //17 randevu no ile eslesen randevu bilgilerini yazdir
+    public void printApp(){
+       boolean isExist=true;
+        System.out.println("Randevu No Giriniz");
+        int appId=input.nextInt();
+        for (Appointment app:this.appointments) {
+            if (app.getId()==appId){
+                isExist=true;
+                System.out.println("--------------------------------------------------");
+                System.out.println(appId+ " No`lu Randevu Bilgileri = ");
+                System.out.println("Hasta Adi : "+app.getName());
+                System.out.println("Doktorun Bransi : "+app.getDoctor().getDepartment());
+                System.out.println("Doktorun Adi : "+app.getDoctor().getName());
+                System.out.println("Randevu Zamani : "+app.getDate());
+                System.out.println("--------------------------------------------------");
+                break;
+            }else {
+                isExist=false;
+
+            }
+
+        }
+        if (!isExist) {
+            System.out.println("Randevu Bulunamadi");
+        }
+
+    }
+
 
 }
--- src/AppointmentApp/Start.java
+++ src/AppointmentApp/Start.java
@@ -7,32 +7,32 @@ public class Start {
     //1.Adim Ana Menu Olusturuldu
     //Doktor ve Randevu icin appointment classlarini olusturacagiz.
     public static void start() {
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        AppointmentService appointmentService1 = new AppointmentService();
+        DoctorService doctorService1 = new DoctorService();
         System.out.println("Merhaba Randevu Sistemine Hosgeldiniz");
         int select;
         do {
             System.out.println("1-Randevu Al");
-            System.out.println("2-Randevu   Goruntule");
+            System.out.println("2-Randevu Goruntule");
             System.out.println("0-Cikis");
             System.out.println("Seciminiz");
-            select=input.nextInt();
-            switch (select){
+            select = input.nextInt();
+            switch (select) {
                 case 1:
-                    //randevu
+                    appointmentService1.getAppointment(doctorService1);
                     break;
                 case 2:
-                    //randevu goruntule
+                    appointmentService1.printApp();
                     break;
-                case  0:
-                    //iyi gunler
+                case 0:
+                    System.out.println("Iyi Gunler");
                 default:
                     System.out.println("Hatali Giris Tekrar Deneyiniz");
                     break;
             }
 
-        }while (select!=0);
-
-
+        } while (select != 0);
 
 
     }
